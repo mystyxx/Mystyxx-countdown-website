b8991a02c29965605a0fc16e78e0b83010c3de89
mystyxx <paul.rouxb@proton.me>
2023-12-29 21:45:41 +0100

    user can select custom date and subtext

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-let countdown = document.getElementById("countdown")
+let countdown = document.getElementById("countdown");
+let customCountDownDate = document.getElementById("customDate");
 let countDownDate = new Date("Mar 5, 2024 00:00:00").getTime();
 let subtext = "until mystyxx' birthday";
 let subtextHTMLElement = document.getElementById("subtext");
+let untilOrSince = 'until'
 
 function showCurrentTime() {
+    updateCustomData();
     let now = new Date().getTime();
     var distance = countDownDate - now;
 
-    let days = Math.floor(distance / (1000*60*60*24));
-    let hours = Math.floor(distance % (1000*60*60*24) / (1000*60*60)) - 1;
-    let minutes = Math.floor(distance % (1000*60*60) / (1000*60));
+    let days = Math.abs(Math.floor(distance / (1000*60*60*24)));
+    let hours = Math.abs(Math.floor(distance % (1000*60*60*24) / (1000*60*60)) - 1);
+    let minutes = Math.abs(Math.floor(distance % (1000*60*60) / (1000*60)));
     let seconds = Math.floor(distance % (1000*60) / 1000);
+
+    if(seconds>0) {
+        untilOrSince = 'until';
+    }
+    else{
+	untilOrSince = 'since';
+        seconds = Math.abs(seconds);
+    }
 
     countdown.innerText = days + ' days ' + hours + 'h ' + minutes + 'mn ' + seconds + 's';
 }
 
-function updateSubtext(newSubtext) {
-    subtext = newSubtext;
-    subtextHTMLElement.innerText = subtext;
+function updateCustomData() {
+    countDownDate = new Date(customCountDownDate.value);
+    subtextHTMLElement.innerText = untilOrSince + ' ' + subtextHTMLElementInput.value;
 }
 
-updateSubtext(subtext);
+updateCustomData();
 let update = setInterval(showCurrentTime, 1000);
